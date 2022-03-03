89fa75ae1d703092000543671b08c97902e930f4
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-03-03 16:08:37 +0545

    setup aboutus section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
             <div className="app__navbar-smallscreen">
                 <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
                 { toggleMenu && (
-                    <div className="app__navbar-smallscreen_overlay flex__container slide-bottom">
+                    <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
                         <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
                         <ul className="app__navbar-smallscreen_links">
                             <li className="p__opensans"><a href="#home">Home</a></li>
--- src/containers/AboutUs/AboutUs.jsx
+++ src/containers/AboutUs/AboutUs.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
-const AboutUs = () => {
-  return (
-    <div>AboutUs</div>
-  )
-}
+import { images } from '../../constants'
+import './AboutUs.css'
+
+const AboutUs = () => (
+    <div className="app__aboutus app__bg flex__center section__padding">
+        <div className="app__aboutus-overlay flex__center">
+            <img src={images.G} alt="g_letter" />
+        </div>
+        <div className="app__aboutus-content flex__center">
+            <div className="app__aboutus-content_about">
+                <h1 className="headtext__cormorant">About Us</h1>
+                <img src={images.spoon} alt="about_spoon" />
+                <p className="p__cormorant">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+                <button className="custom__button" type="button">Know More</button>
+            </div>
+            <div className="app__aboutus-content_knife">
+                <img src={images.knife} alt="about_knife" />
+            </div>
+            <div className="app__aboutus-content_history">
+                <h1 className="headtext__cormorant">Our History</h1>
+                <img src={images.spoon} alt="about_spoon" />
+                <p className="p__cormorant">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+                <button className="custom__button" type="button">Know More</button>
+            </div>
+        </div>
+    </div>
+)
 
 export default AboutUs
